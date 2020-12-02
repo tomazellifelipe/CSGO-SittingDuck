70ad2bdce827c901f139b10c3dcc2a74c0cb4fde
Felipe Tomazelli <fefetl08@gmail.com>
2020-12-02 17:23:47 -0300

    feat: basic plot function

--- src/hello.py
+++ src/hello.py
@@ -1,9 +1,26 @@
-from flask import Flask, request, render_template
-from player import Player
+import matplotlib.pyplot as plt
+import pandas as pd
+from flask import Flask, render_template, request
+
+from player import Map, Player
 
 app = Flask(__name__)
-userinfo = dict()
-player = Player('76561198263058338')
+userinfo = [0]
+userdata = dict()
+positionaldata = {'pos_x': [], 'pos_y': [], 'pos_z': []}
+
+
+def plotPosition(userdata):
+    position = userdata['position']
+    for pos in position:
+        x, y, z = pos.split(",")
+        positionaldata['pos_x'].append(float(x.strip()))
+        positionaldata['pos_y'].append(float(y.strip()))
+        positionaldata['pos_z'].append(float(z.strip()))
+    df = pd.DataFrame(positionaldata)
+    plt.plot(df['pos_x'], df['pos_y'], 'ro')
+    plt.axis([-4831,  494, -3542, 1781])
+    plt.show()
 
 
 @app.route('/')
@@ -14,22 +31,28 @@ def home():
 
 @app.route('/cslog')
 def csgo():
-    return render_template('userinfo.html', userinfo=userinfo)
+    return render_template('userinfo.html', userinfo=userdata)
 
 
 @app.route('/api/csgo', methods=['POST'])
 def csgoapi():
-    positionsJson = request.get_json()
-    player.position.append(
-        positionsJson['allplayers'][player.steamid]['position'])
-    userinfo.update(positionsJson)
-    return 'This is a POST route for CS:GO Game Integration'
+    data = request.get_json()
+    _player = Player(data['player']['steamid'], data['player']['name'])
+    if data['player']['activity'] == 'playing':
+        _map = Map(data['map']['mode'], data['map']['name'])
+        _map.phase = data['map']['phase']
+        if _map.phase == 'live':
+            _player.position.append(
+                data['allplayers'][_player.steamid]['position'])
+    userdata.update(_player.__dict__)
+    print(userdata)
+    return 'ok'
 
 
 @app.route('/debug')
 def debug():
-    print(player.position)
-    return 'Ok'
+    plotPosition(userdata)
+    return render_template('debug.html')
 
 
 if __name__ == '__main__':
--- src/player.py
+++ src/player.py
@@ -1,4 +1,34 @@
-class Player:
-    def __init__(self, id) -> None:
+class SingletonMeta(type):
+    """
+    The Singleton class can be implemented in different ways in Python. Some
+    possible methods include: base class, decorator, metaclass. We will use the
+    metaclass because it is best suited for this purpose.
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        """
+        Possible changes to the value of the `__init__` argument do not affect
+        the returned instance.
+        """
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class Player(metaclass=SingletonMeta):
+    def __init__(self, id, name) -> None:
         self.steamid = id
+        self.name = name
         self.position = list()
+
+
+class Map(metaclass=SingletonMeta):
+    def __init__(self, mode, name) -> None:
+        self.mode = mode
+        self.name = name
+        self.initial_x = 0
+        self.initial_y = 0
+    pass
